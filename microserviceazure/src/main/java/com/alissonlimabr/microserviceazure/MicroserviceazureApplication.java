fe4a2549a628c28867386bc05ebb4c0676a1ef86
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-06 21:52:52 -0500

    spring initializr

---
+++ microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/MicroserviceazureApplication.java
@@ -0,0 +1,13 @@
+package com.alissonlimabr.microserviceazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceazureApplication.class, args);
+	}
+
+}
---
+++ microserviceazure/src/test/java/com/alissonlimabr/microserviceazure/MicroserviceazureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonlimabr.microserviceazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceazureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
