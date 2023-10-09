12aa1b632fea2a76924679c243316d1e9d966b8b
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-08 19:28:44 -0500

    add JwtService + Implementações

---
+++ microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/service/JwtService.java
@@ -0,0 +1,9 @@
+package com.alissonlimabr.microserviceazure.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+    String extractUserName(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
