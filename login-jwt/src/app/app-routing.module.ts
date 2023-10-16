8934e81901be968db9247eb9d57312a9b21c5c66
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-16 13:00:50 -0500

    Atualiza rotas

--- login-jwt/src/app/app-routing.module.ts
+++ login-jwt/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SendEmailFormComponent } from './components/send-email-form/send-email-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: SendEmailFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
