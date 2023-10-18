036d5416d7c8b2d6f275066e43903e08e80687e8
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-18 10:20:41 -0500

    Atualiza Origins no filtro CORS

--- microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/config/CorsConfig.java
+++ microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
@@ -14,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedOrigins(Arrays.asList("www.loginjwt.alissonlimadev.com", "loginjwt.alissonlimadev.com", "login-jwt-angular.vercel.app"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
