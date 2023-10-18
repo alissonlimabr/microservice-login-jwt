f575663721ebf39e9df6370a1d40ac564fb3e274
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-18 02:04:31 -0500

    Add services + integração com api

--- login-jwt/src/app/services/loading.service.ts
+++ login-jwt/src/app/services/loading.service.ts
@@ -6,7 +6,6 @@ import { BehaviorSubject, Subject } from 'rxjs';
 })
 export class LoadingService {
   constructor() {}
-
   private _loading = false;
   loadingStatus = new BehaviorSubject<boolean>(false);
 
--- login-jwt/src/app/services/user.service.ts
+++ login-jwt/src/app/services/user.service.ts
@@ -1,19 +1,46 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../environments/environment.prod';
 import { User } from '../models/user';
-import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  public autenticado = false;
 
+  constructor(private http: HttpClient, private router: Router) {}
   // Url da api
   apiUrl: string = environment.apiUrl;
-
   // Cadastro de usuário
   insert(objeto: User): Observable<User> {
-    return this.http.post<User>(this.apiUrl + 'signup', objeto);
+    return this.http.post<User>(this.apiUrl + 'auth/signup', objeto);
+  }
+  // Cadastro de usuário
+  login(objeto: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl + 'auth/signin', objeto).pipe(
+      tap((token) => {
+        if (token) {
+          sessionStorage.setItem('token', JSON.stringify(token));
+          this.router.navigate(['/home']);
+        }
+      })
+    );
+  }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    this.router.navigate(['']);
+  }
+
+  getHome() {
+    const token = JSON.parse(sessionStorage.getItem('token') || '{}');
+    let reqHeader = new HttpHeaders({
+      Authorization: 'Bearer ' + token.token,
+    });
+    return this.http.get(this.apiUrl + 'resource', {
+      headers: reqHeader,
+    });
   }
 }
