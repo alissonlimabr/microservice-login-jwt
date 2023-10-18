f812c53129147af499178595d26c810ac9d71293
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-18 02:07:14 -0500

    Add Environment para conexão com a api

--- login-jwt/src/app/environments/environment.prod.ts
+++ login-jwt/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://loginjwtapi.alissonlimadev.com/jwt/v1/auth/',
+  apiUrl: 'http://loginjwtapi.alissonlimadev.com/jwt/v1/',
 };
--- login-jwt/src/app/environments/environment.ts
+++ login-jwt/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/jwt/v1/auth/',
+  apiUrl: 'http://localhost:8080/jwt/v1/',
 };
