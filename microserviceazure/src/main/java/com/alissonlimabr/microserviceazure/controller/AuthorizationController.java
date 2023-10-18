535b47f40ef59e4f0dba02fcd62dc06a1a5306d9
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-18 02:15:21 -0500

    Refatora endpoint de autorização

--- microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/controller/AuthorizationController.java
+++ microserviceazure/src/main/java/com/alissonlimabr/microserviceazure/controller/AuthorizationController.java
@@ -1,5 +1,6 @@
 package com.alissonlimabr.microserviceazure.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthorizationController {
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Aqui está o seu recurso");
+    public ResponseEntity<String> loginAutorizado() {
+        return new ResponseEntity<>("Login Autorizado", HttpStatus.OK);
     }
 }
