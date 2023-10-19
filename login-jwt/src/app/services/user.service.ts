a824849e4635646c88c91838107c03c776abae6f
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-19 02:38:53 -0500

    Atualiza métodos + endpoint de user.service

--- login-jwt/src/app/services/user.service.ts
+++ login-jwt/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../environments/environment.prod';
+import { environment } from '../environments/environment';
 import { User } from '../models/user';
 @Injectable({
   providedIn: 'root',
@@ -42,5 +42,30 @@ export class UserService {
     return this.http.get(this.apiUrl + 'resource', {
       headers: reqHeader,
     });
+  }
+
+  // Conexão com endpoint para reset/update de senha do usuário. Passa o token e a nova senha para verificações e update pelo back-end.
+  update(
+    token: string,
+    newPasswordWithEmail: { email: string; newPassword: string }
+  ): Observable<void> {
+    // Define o cabeçalho do tipo json.
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    // conexão com a url da api
+    const url = `${this.apiUrl}auth/reset-password/${token}`;
+    return this.http.post<void>(url, newPasswordWithEmail, { headers });
+  }
+
+  sendEmail(email: User): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post(
+      this.apiUrl + 'auth/reset-password/forgot?email=' + email.email,
+      {
+        headers: headers,
+      }
+    );
   }
 }
