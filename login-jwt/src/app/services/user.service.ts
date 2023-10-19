47a8e88ef0ed8c6a2937e2adcc79e8ba5cdcbda2
Alisson Lima <101370736+alissonlimabr@users.noreply.github.com>
2023-10-19 02:58:07 -0500

    Atualiza environments

--- login-jwt/src/app/services/user.service.ts
+++ login-jwt/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { User } from '../models/user';
 @Injectable({
   providedIn: 'root',
