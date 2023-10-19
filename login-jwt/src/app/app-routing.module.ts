aa30af28582e148efd82710ec3dadc4278f12d03
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-19 02:39:32 -0500

    Add novas rotas

--- login-jwt/src/app/app-routing.module.ts
+++ login-jwt/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SendEmailFormComponent } from './components/send-email-form/send-email-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { HomeComponent } from './components/home/home.component';
+import { SendEmailResetComponent } from './components/send-email-reset/send-email-reset.component';
+import { ResetPasswordFormComponent } from './components/reset-password-form/reset-password-form.component';
 
 const routes: Routes = [
-  { path: '', component: SendEmailFormComponent },
+  { path: '', component: LoginFormComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'forgot', component: SendEmailResetComponent },
+  {
+    path: 'reset/:email/:token',
+    component: ResetPasswordFormComponent,
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
